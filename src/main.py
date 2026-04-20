d8971214da6577fad4da186e3fa632cceec7a058
Claylton-Muniz <claymuniz67@gmail.com>
2026-04-20 19:39:04 +0000

    Criando um código e diagrama básico para testar o funcionamento do sistema

--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
-print("Teste")
+import machine
+import time
+
+led = machine.Pin(23, machine.Pin.OUT)
+
+print("Iniciando o teste do LED...")
+
+
+while True:
+    led.value(1)
+    print("LED Ligado")
+    time.sleep(1)
+    
+    led.value(0)
+    print("LED Desligado")
+    time.sleep(1)
