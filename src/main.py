cc5241982b93da15192d876614981301d81e2d94
Claylton-Muniz <claymuniz67@gmail.com>
2026-04-21 03:12:00 +0000

    Adição de uma tela oled e correção no dockerfile para ler outros arquivos na pasta src

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
-from machine import Pin
+from machine import Pin, I2C
 from dht import DHT22
-import time
+from time import sleep
+from ssd1306 import SSD1306_I2C
 
 # Configuração do Sensor DHT22 no pino 32
 sensor = DHT22(Pin(32))
+
+# Configuração do I2C para o display OLED
+i2c = I2C(0, scl=Pin(18), sda=Pin(19)) 
+
+oled_width = 128
+oled_height = 64
+oled = SSD1306_I2C(oled_width, oled_height, i2c)
 
 # Lista de pinos da Barra de LEDs organizado pelas cores (verde, amarelo, vermelho)
 # Verde: 21, 22, 23 - Amarelo: 13, 12, 14 - Vermelho: 27, 26, 25, 33
@@ -42,8 +50,11 @@ while True:
         
         print(f"Temperatura atual do Freezer: {temp}°C")
         atualizar_barra_de_leds(temp)
+
+        oled.text('Hello, Wokwi!', 10, 10)      
+        oled.show()
         
     except OSError as e:
         print("Falha ao ler o sensor DHT22!!!")
         
-    time.sleep(1)
+    sleep(1)
